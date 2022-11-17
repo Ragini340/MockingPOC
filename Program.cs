8c3c11d4ed31a28f00dbd202925b74bace4718a1
Ragini340 <ragini31bxr@gmail.com>
2022-11-17 16:56:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockingPOC
+{
+
+    public class checkEmployee
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public virtual Boolean checkEmp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class processEmployee
+        {
+            public Boolean insertEmployee(checkEmployee objtmp)
+            {
+                objtmp.checkEmp();
+                return true;
+            }
+        }
+    } 
+}
+
