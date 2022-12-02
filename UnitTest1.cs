9fd94c4a7e1236045ba803936a56caa926ab373a
Ragini340 <88105537+Ragini340@users.noreply.github.com>
2022-12-02 16:47:42 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockingPOC;
+using Moq;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod("Check")]
+        
+        public void TestMethod1()
+        {
+            Console.WriteLine("I am under TestMethod1");
+            Mock<MockingPOC.checkEmployee> chk = new Mock<checkEmployee>();
+            
+            //TestC.WriteLine("I am in TestMethod1" + chk);
+             //Console.WriteLine("Test in Test"+chk);
+
+            checkEmployee check = new checkEmployee();
+            //Console.WriteLine("Test in Test real" + check);
+
+
+            // Console.WriteLine("Test in Test" + chk.checkEmp());
+
+            //Console.WriteLine("I am in TestMethod1");
+            chk.Setup(x => x.checkEmp()).Returns(true);
+
+            checkEmployee.processEmployee obje = new checkEmployee.processEmployee();
+            Assert.AreEqual(obje.insertEmployee(chk.Object), true);
+
+
+        }
+    }
+}
